bb107c6ec03119fa0d9ff78517d81b33ec69e228
Simran Verma <vermasimran327@gmail.com>
2025-12-06 10:09:54 +0530

    new

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -53,7 +53,7 @@ async function registerUser(req, res) {
 async function loginUser(req, res) {
     const { email, password } = req.body;
 
-    const user = await userModel.findOne({ email })
+    const user = await userModel.findOne({ email }).select('+password')
 
     if (!user) {
         return res.status(400).json({ message: "Invalid email or password" })
